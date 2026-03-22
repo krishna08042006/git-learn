e9ef59f5f4e1134bf4c15545e19c688d403aeca0
Krishna <krishna6785kumar@gmail.com>
2026-03-22 21:28:36 +0530

    one more print added

--- gitLearn.py
+++ gitLearn.py
@@ -80,3 +80,5 @@ print("git learning in a great way")
 print("should be able to do something")
 
 print("something to consider to happen in a best way")
+
+print("something to consider in a best way to consider it would be best")
