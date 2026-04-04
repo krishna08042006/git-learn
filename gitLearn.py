dc4799d652a01be03e85fa419ea06a7d0a8e2560
Krishna <krishna6785kumar@gmail.com>
2026-04-04 23:54:19 +0530

    another print added

--- gitLearn.py
+++ gitLearn.py
@@ -116,3 +116,5 @@ print("Git learing to do something in a great way")
 print("Git learned to be best in git")
 
 print("Git learning is best when learned perfectly")
+
+print("Git is learning in best way")
