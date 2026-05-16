0f89841487c08c19821791f169f29bd9db7232b5
Krishna <krishna6785kumar@gmail.com>
2026-05-16 17:54:41 +0530

    one more print added

--- gitLearn.py
+++ gitLearn.py
@@ -182,3 +182,5 @@ print("git is best in its best pace of learning")
 print("git is best learned when it is best to be learned")
 
 print("git is learned to transform something")
+
+print("git is best to be learned in it's best pace of time")
