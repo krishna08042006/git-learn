207c8acb987eeb7bd889713045868c0c24bd9b3b
Krishna <krishna6785kumar@gmail.com>
2026-06-19 19:45:09 +0530

    one more print added

--- gitLearn.py
+++ gitLearn.py
@@ -228,3 +228,5 @@ print("git is best when it is learned in right path")
 print("git is best when it is learned and adjusted in a best way")
 
 print("git is to be best learned in a best way")
+
+print("git is to be learn't in a best possible way to accomplish something that could be best")
